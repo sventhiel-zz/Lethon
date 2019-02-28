6a6cb1a0b1fcf4adc500abb3265435d58ecae471
Sven Thiel <sven.thiel@uni-jena.de>
2019-02-28 16:50:38 +0100

    add reamining functions, so that all from crypto-loot.com are supported

--- Lethon/Interfaces/IUserService.cs
+++ Lethon/Interfaces/IUserService.cs
@@ -4,8 +4,30 @@ using Lethon.Models;
 
 namespace Lethon.Interfaces
 {
+    /// <summary>
+    /// User service.
+    /// </summary>
     public interface IUserService
     {
+        /// <summary>
+        /// Balances the async.
+        /// </summary>
+        /// <returns>The async.</returns>
+        /// <param name="requestModel">Request model.</param>
         Task<BalanceResponse> BalanceAsync(BalanceRequest requestModel);
+
+        /// <summary>
+        /// Tops the async.
+        /// </summary>
+        /// <returns>The async.</returns>
+        /// <param name="requestModel">Request model.</param>
+        Task<TopResponse> TopAsync(TopRequest requestModel);
+
+        /// <summary>
+        /// Lists the async.
+        /// </summary>
+        /// <returns>The async.</returns>
+        /// <param name="requestModel">Request model.</param>
+        Task<ListResponse> ListAsync(ListRequest requestModel);
     }
 }
