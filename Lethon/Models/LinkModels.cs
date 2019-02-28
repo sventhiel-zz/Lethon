f3cde17c655a70077db643e952f8ccffcca56117
Sven Thiel <sven.thiel@uni-jena.de>
2019-02-28 16:46:21 +0100

    add comments for documentation, finalized the whole model (hopefully)

--- Lethon/Models/LinkModels.cs
+++ Lethon/Models/LinkModels.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 
 namespace Lethon.Models
 {
+    /// <summary>
+    /// Link request.
+    /// </summary>
     public class LinkRequest
     {
         public string Secret { get; set; }
         public string URL { get; set; }
         public int Hashes { get; set; }
 
+        /// <summary>
+        /// Tos the dictionary.
+        /// </summary>
+        /// <returns>The dictionary.</returns>
         public Dictionary<string, string> ToDictionary()
         {
             return new Dictionary<string, string> { { "secret", Secret }, { "url", URL }, { "hashes", Hashes.ToString() } };
         }
     }
 
+    /// <summary>
+    /// Link response.
+    /// </summary>
     public class LinkResponse
     {
         public string Error { get; set; }
