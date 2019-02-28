ed4fdf15625b4fb13eb8d9b5efe655d180f91bc9
Sven Thiel <sven.thiel@uni-jena.de>
2019-02-28 16:49:15 +0100

    add add last function to support all listed ones from crypto-loot.com

--- Lethon/Interfaces/IStatsService.cs
+++ Lethon/Interfaces/IStatsService.cs
@@ -4,8 +4,23 @@ using System.Threading.Tasks;
 using Lethon.Models;
 namespace Lethon.Interfaces
 {
+    /// <summary>
+    /// Stats service.
+    /// </summary>
     public interface IStatsService
     {
+        /// <summary>
+        /// Histories the async.
+        /// </summary>
+        /// <returns>The async.</returns>
+        /// <param name="requestModel">Request model.</param>
         Task<List<HistoryResponse>> HistoryAsync(HistoryRequest requestModel);
+
+        /// <summary>
+        /// Payouts the async.
+        /// </summary>
+        /// <returns>The async.</returns>
+        /// <param name="requestModel">Request model.</param>
+        Task<PayoutResponse> PayoutAsync(PayoutRequest requestModel);
     }
 }
